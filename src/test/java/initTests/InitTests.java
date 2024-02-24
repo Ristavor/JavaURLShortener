4415b77f53a0e08149f6dadcefff435b488dcde4
Ristavor <peampate@gmail.com>
2024-02-24 15:12:58 +0300

    first commit

---
+++ src/test/java/initTests/InitTests.java
@@ -0,0 +1,11 @@
+package initTests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class InitTests {
+    @Test
+    public void initTest() {
+        assertTrue(true);
+    }
+}
